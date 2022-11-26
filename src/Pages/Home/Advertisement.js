6bd9b71aeedd9313d8356ec4cacea66239cb5b5a
Shapna <shapna088@gmail.com>
2022-11-27 03:21:55 +0600

    add

--- src/Pages/Home/Advertisement.js
+++ src/Pages/Home/Advertisement.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Advertisement = () => {
     return (
         <div className='text-center '>
-            <h2 className='font-bold text-2xl text-teal-700 mt-12'>Advertisement Items Section</h2>
-            <p>Right now no advertise here</p>
+            {/* <h2 className='font-bold text-2xl text-teal-700 mt-12'>Advertisement Items Section</h2>
+            <p>Right now no advertise here</p> */}
         </div>
     );
 };
