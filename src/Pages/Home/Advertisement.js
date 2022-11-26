e718f560f9743628a1d37546b4241871744793ff
Shapna <shapna088@gmail.com>
2022-11-26 17:08:45 +0600

    advertisement section added

---
+++ src/Pages/Home/Advertisement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Advertisement = () => {
+    return (
+        <div className='text-center '>
+            <h2 className='font-bold text-2xl text-teal-700 mt-12'>Advertisement Items Section</h2>
+            <p>Right now no advertise here</p>
+        </div>
+    );
+};
+
+export default Advertisement;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Advertisement from './Advertisement';
 import Banner from './Banner';
 import Categories from './Categories';
 import DeliveryOptions from './DeliveryOptions';
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Categories></Categories>
+            <Advertisement></Advertisement>
             <DeliveryOptions></DeliveryOptions>
         </div>
     );
