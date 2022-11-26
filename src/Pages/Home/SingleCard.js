a0178fd70982a85111b23438404755b7409ceef5
Shapna <shapna088@gmail.com>
2022-11-26 18:50:52 +0600

    verified seller

--- src/Pages/Home/SingleCard.js
+++ src/Pages/Home/SingleCard.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Contexts/AuthProvider';
+import { FaCheckSquare } from 'react-icons/fa';
+
 
 const SingleCard = ({ detail, setBookFurniture }) => {
-    const { category_id, image, location, original_price, published_date, resale_Price, seller_name, title, used_time, verified_seller, _id } = detail;
+    const { category_id, image, location, original_price, published_date, resale_Price, seller_name, title, used_time, role, _id } = detail;
+    const { user } = useContext(AuthContext)
     return (
         <div>
             <div className="rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
                 <img src={image} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
                 <div className="flex flex-col justify-between p-6 space-y-8">
                     <div className="space-y-2">
-                        <h2 className="text-3xl font-semibold tracking-wide">{title}</h2>
+                        <h2 className="text-3xl font-semibold tracking-wide flex gap-2">
+                            <span>{title}</span>
+                            <span>{role === 'verified' && <FaCheckSquare className='bg-blue-700 text-white'></FaCheckSquare>}</span>
+                        </h2>
                         <p className="dark:text-gray-100">Original Price: ${original_price}</p>
                         <p className="dark:text-gray-100">Selling Price: ${resale_Price}</p>
                         <p className="dark:text-gray-100">Used_time: {used_time}</p>
