56e10457178618a6be40c1c2d765133530386c02
shapna28 <shapna088@gmail.com>
2024-01-23 17:08:26 +0600

    add

--- src/Pages/Home/Advertisement.js
+++ src/Pages/Home/Advertisement.js
@@ -21,25 +21,28 @@ const Advertisement = () => {
         }
     })
 
-    if(isLoading){
+    if (isLoading) {
         return <Spinner></Spinner>
     }
 
     return (
         <>
+            <div className='text-center my-12'>
+                <h2 className='font-bold text-2xl text-[#67AD5C]'>Advertisement Products</h2>
+                <p className='text-sm font-semibold'>Advertise going on</p>
+            </div>
             <div className='text-center my-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4'>
                 {
                     products?.map(product => product.status === true &&
-                            <div key={product._id} className="card bg-base-100 shadow-xl">
-                                <figure>
-                                    <img src={product.image} alt="" className="rounded-xl" />
-                                </figure>
-                                <div className="card-body items-center text-center">
-                                <h2 className='font-bold text-2xl text-[#67AD5C]'>Advertise Item </h2>
-                                    <h2 className="card-title">{product.product}</h2>
-                                    <p>Price: {product.price}</p>
-                                </div>
+                        <div key={product._id} className="card">
+                            <figure>
+                                <img src={product.image} alt="" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{product.product}</h2>
+                                <p>Price: {product.price}</p>
                             </div>
+                        </div>
                     )
                 }
             </div>
